a18671f7858d7d60fffb24d3f9ad14ce0b71ad9c
Prashant Varma <prashantvarma5083@gmail.com>
2024-09-26 03:11:21 +0530

    project clean

--- backend-odds/src/routes/admin.ts
+++
@@ -1,8 +0,0 @@
-import express, { Router } from "express";
-import { newOrder } from "../controllers/user";
-const router:Router = express.Router();
-
-
-router.post("/create-event", newOrder);
-
-export default router;
