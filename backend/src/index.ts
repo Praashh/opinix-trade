b0cd2920553d23951b98702bfb9cb569c9528f33
Aryan Pachori <aryanpachori03@gmail.com>
2024-09-26 20:59:03 +0530

    update index.ts

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import orderRouter from "./router/order";
+
+import http from "http";
+import { setupwebsocket } from "./router/websockets";
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/v1", orderRouter);
+const server = http.createServer(app);
+export const WebsocketServer = setupwebsocket(server);
+server.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
