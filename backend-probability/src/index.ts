b428e465f0849d7de4c97e25a62b4d5c3b6858a9
Aryan Pachori <aryanpachori03@gmail.com>
2024-09-26 20:26:19 +0530

    Schema init

--- backend-probability/src/index.ts
+++ backend-probability/src/index.ts
@@ -1,20 +1,18 @@
-
-import express from "express"
+import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import orderRouter from "./router/order";
 
-import http from 'http';
+import http from "http";
 import { setupwebsocket } from "./router/websockets";
+
 const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use("/v1",orderRouter)
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/v1", orderRouter);
 const server = http.createServer(app);
-export const WebsocketServer = setupwebsocket(server)
- server.listen(3000, () => {
-    console.log(`Server is running on http://localhost:3000`);
-  });
-
-  
+export const WebsocketServer = setupwebsocket(server);
+server.listen(3000, () => {
+  console.log(`Server is running on http://localhost:3000`);
+});
