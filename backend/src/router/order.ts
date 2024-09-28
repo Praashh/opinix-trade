2c7b3d8fb681d29caf1cd2c079532eb1b0288371
Aryan Pachori <aryanpachori03@gmail.com>
2024-09-28 17:03:48 +0530

    Backend refactor

--- backend/src/router/order.ts
+++ backend/src/router/order.ts
@@ -1,42 +1,25 @@
-// import { Router } from "express";
-// import {
-//   calculateProbabilty,
-//   getPortfolio,
-//   orderBook,
-//   processOrder,
-// } from "../utils/marketMaker";
-// const router = Router();
+import { Router } from "express";
 
-// router.get("/orderbook", async (req, res) => {
-//   const probability = calculateProbabilty(orderBook);
-//   res.json({
-//     orderBook,
-//     probability,
-//   });
-// });
+const router = Router();
 
-// router.post("/order", (req, res) => {
-//   const { side, quantity, price } = req.body;
 
-//   if (!["yes", "no"].includes(side) || !quantity || !price) {
-//     return res.status(400).json({ error: "Invalid order data" });
-//   }
+router.post("/place-order", (req, res) => {
+  const { eventId ,side, quantity, price } = req.body;
 
-//   const result = processOrder(side, quantity, price, orderBook);
+  if ( !eventId ||!["yes", "no"].includes(side) || !quantity || !price) {
+    return res.status(400).json({ error: "Invalid order data" });
+  }
 
-//   if (!result.success) {
-//     return res.status(400).json({ error: result.message });
-//   }
+  try{
 
-//   res.json({ message: "Order processed successfully." });
-// });
+  }catch(e){
+    console.log("Error placing order",e)
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 
-// router.get("/portfolio", (req, res) => {
-//   const portfolio = getPortfolio();
-//   if (!portfolio.success) {
-//     return res.status(400).json({ message: portfolio.message });
-//   }
-//   return res.json(portfolio);
-// });
 
-// export default router;
+});
+
+
+
+export default router;
