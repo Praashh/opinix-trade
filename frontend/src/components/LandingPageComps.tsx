10bd1cfe377eb03dbc0d64e17d47839924673b53
saiabhiramjaini <abhiramjaini28@gmail.com>
2024-09-29 16:49:41 +0530

    added navbar and footer

---
+++ frontend/src/components/LandingPageComps.tsx
@@ -0,0 +1,252 @@
+import React from "react";
+import { MdArrowOutward } from "react-icons/md";
+import { CiFacebook } from "react-icons/ci";
+import { FaInstagram } from "react-icons/fa6";
+import { BsTwitterX } from "react-icons/bs";
+import { FaLinkedin } from "react-icons/fa";
+import bgGradient from "../assets/image.svg";
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+interface DashboardImageProps {
+  src: string;
+}
+
+interface TransactionInfoProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+interface TradingOpportunityProps {
+  title: string;
+  description: string;
+  image: string;
+}
+
+interface TradingToolProps {
+  title: string;
+  description: string;
+  image: string;
+  isReversed: boolean;
+}
+
+interface CustomerReviewProps {
+  review: string;
+  name: string;
+  position: string;
+  image: string;
+}
+
+export const LandingNavbar = () =>{
+  return(
+    <div className="w-full h-24 bg-[#05071A] p-5 flex justify-between">
+      {/* OpinioX Logo */}
+      <div className="text-white font-sora font-semibold text-4xl">
+        OpinioX
+      </div>
+
+      <div className="flex items-center space-x-4 pr-10">
+        <div className="text-gray font-sora">
+          Features
+        </div>
+
+        <div className="text-gray font-sora">
+          Reviews
+        </div>
+
+        <div className="text-gray font-sora">
+          FAQ's
+        </div>
+      </div>
+      
+
+      <button className="w-[160px] h-[40px] bg-gradient-to-r from-[#212635] to-[#191B25] rounded font-sora font-semibold text-md text-white">
+          SIGN UP
+        </button>
+    </div>
+  )
+} 
+
+export const HeroSection: React.FC = () => (
+  <div className="flex flex-col pt-[150px] items-center justify-center">
+    <div className="text-gray font-extralight font-sora text-center px-3 py-1 rounded-full shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]">
+      Mobile app available. Download now.
+    </div>
+    <div className="text-white font-sora text-6xl text-center mt-14">
+      Trade Your Opinion using <br /> OpiniX
+    </div>
+    <div className="text-gray font-sora mt-3 w-1/2 text-center">
+      Join a dynamic platform where you can predict outcomes, buy and sell
+      shares on various events, and profit from your insights. Turn your
+      opinions into real value and engage in real-time markets today!
+    </div>
+    <Button text="Get Started" onClick={() => {}} />
+  </div>
+);
+
+export const Button: React.FC<ButtonProps> = ({ text, onClick }) => (
+  <button
+    onClick={onClick}
+    className="bg-blue-500 text-white font-semibold rounded-[10px] font-sora px-5 py-3 m-8"
+  >
+    <span className="flex gap-2">
+      {text} <MdArrowOutward className="text-xl" />
+    </span>
+  </button>
+);
+
+export const DashboardImage: React.FC<DashboardImageProps> = ({ src }) => (
+  <div className="w-screen">
+    <img src={src} alt="" className="w-full absolute top-[100px]" />
+    <div className="relative flex justify-center top-[200px]">
+      <div className="p-3 border border-gray rounded-xl w-[1000px]">
+        <img src={src} alt="" className="rounded-xl" />
+      </div>
+    </div>
+  </div>
+);
+
+export const TransactionInfo: React.FC<TransactionInfoProps> = ({
+  icon,
+  title,
+  description,
+}) => (
+  <div className="p-5 rounded-xl shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]">
+    <div className="flex justify-center mt-5">
+      <div className="w-14 h-14 bg-[#101636] rounded-full">
+        <img src={icon} alt="" className="w-10 h-10 mt-2 ml-2" />
+      </div>
+    </div>
+    <div className="text-white text-xl font-sora text-center my-3">{title}</div>
+    <div className="text-gray text-center text-xs font-sora mb-5">
+      {description}
+    </div>
+  </div>
+);
+
+export const TradingOpportunity: React.FC<TradingOpportunityProps> = ({
+  title,
+  description,
+  image,
+}) => (
+  <div className="rounded-xl p-10 px-14 shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]">
+    <div className="text-white text-3xl font-sora my-5">{title}</div>
+    <div className="text-gray font-sora text-sm mb-10">{description}</div>
+    <div className="flex justify-center items-center">
+      <img src={image} alt="" className="h-64 w-64 mb-5" />
+    </div>
+  </div>
+);
+
+export const TradingTool: React.FC<TradingToolProps> = ({
+  title,
+  description,
+  image,
+  isReversed,
+}) => (
+  <>
+    {isReversed ? (
+      <>
+        <div>
+          <div className="text-white text-3xl font-sora my-5 mt-10">
+            {title}
+          </div>
+          <div className="text-gray font-sora text-sm mb-10">{description}</div>
+          <button
+            onClick={() => {}}
+            className="bg-blue-500 text-white font-semibold rounded-[10px] font-sora px-5 py-3"
+          >
+            <span className="flex gap-2">
+              Learn More <MdArrowOutward className="text-xl" />
+            </span>
+          </button>
+        </div>
+        <div className="rounded-xl p-10 shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]">
+          <div className="flex justify-center items-center">
+            <img src={image} alt="" className="h-52 w-52" />
+          </div>
+        </div>
+      </>
+    ) : (
+      <>
+        <div className="rounded-xl p-10 shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]">
+          <div className="flex justify-center items-center">
+            <img src={image} alt="" className="h-52 w-52 mb-5" />
+          </div>
+        </div>
+        <div>
+          <div className="text-white text-3xl font-sora my-5 mt-10">
+            {title}
+          </div>
+          <div className="text-gray font-sora text-sm mb-10">{description}</div>
+
+          <button
+            onClick={() => {}}
+            className="bg-blue-500 text-white font-semibold rounded-[10px] font-sora px-5 py-3"
+          >
+            <span className="flex gap-2">
+              Learn More <MdArrowOutward className="text-xl" />
+            </span>
+          </button>
+        </div>
+      </>
+    )}
+  </>
+);
+
+export const CustomerReview: React.FC<CustomerReviewProps> = ({
+  review,
+  name,
+  position,
+  image,
+}) => (
+  <div className="border-2 border-white mt-10 rounded-[30px]">
+    <div className="text-white font-gray font-sora py-10 px-5">{review}</div>
+    <div className="flex justify-between">
+      <div>
+        <div className="text-white font-gray font-sora px-5 mt-2">{name}</div>
+        <div className="text-gray font-gray font-sora px-5">{position}</div>
+      </div>
+      <div>
+        <img src={image} alt="" className="h-20 w-20 rounded-[20px]" />
+      </div>
+    </div>
+  </div>
+);
+
+export const Footer = () => {
+  return (
+    <div className="relative px-20 mt-20 pb-20 bg-cover bg-top" style={{ backgroundImage: `url(${bgGradient}),` }}>
+      <div className="flex gap-10">
+        <div className="text-white font-sora text-2xl">OpinioX</div>
+        <div className="h-8 w-0.5 bg-white"></div>
+        <div className="text-gray font-sora text-sm mt-2">
+          Trade your opinion using OpiniX
+        </div>
+      </div>
+
+      <div className="w-full h-0.5 bg-gray mt-10"></div>
+
+      <div className="flex justify-between p-5">
+        <div>
+          <div className="text-gray font-sora text-sm">
+            Copyright 2024 OpinioX.
+          </div>
+          <div className="text-gray font-sora text-sm">
+            All Rights Reserved.
+          </div>
+        </div>
+        <div className="flex gap-10">
+          <CiFacebook className="text-white h-5 w-5" />
+          <FaInstagram className="text-white" />
+          <BsTwitterX className="text-white" />
+          <FaLinkedin className="text-white" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,14 @@
-import { MdArrowOutward } from "react-icons/md";
-import bgGradient from "../assets/bgGradient.png";
+import React from "react";
+import {
+  HeroSection,
+  TransactionInfo,
+  TradingOpportunity,
+  TradingTool,
+  CustomerReview,
+  Footer,
+  LandingNavbar,
+} from "../components/LandingPageComps";
+import bgGradient from "../assets/image.svg";
 import dashboard from "../assets/dashboard.png";
 import transaction from "../assets/transaction.png";
 import sports from "../assets/sports.png";
@@ -9,40 +18,16 @@ import politics from "../assets/politics.png";
 import orderBook from "../assets/orderBook.png";
 import graph from "../assets/graph.png";
 
-const LandingPage = () => {
+const LandingPage: React.FC = () => {
   return (
     <div className="bg-bg">
-      {/* Hero Section */}
-      <div className="flex flex-col pt-[150px] items-center justify-center w-screen h-screen">
-        <div
-          className="text-gray font-extralight font-sora text-center px-3 py-1 rounded-full
-        shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-        >
-          Mobile app available. Download now.
-        </div>
-        <div className="text-white font-sora text-6xl text-center mt-14">
-          Trade Your Opinion using <br /> OpiniX
-        </div>
-        <div className="text-gray font-sora mt-3 w-1/2 text-center">
-          Join a dynamic platform where you can predict outcomes, buy and sell
-          shares on various events, and profit from your insights. Turn your
-          opinions into real value and engage in real-time markets today!
-        </div>
-        <button
-          onClick={() => {}}
-          className="bg-blue-500 text-white font-semibold rounded-[10px] font-sora px-5 py-3 m-8"
-        >
-          <span className="flex gap-2">
-            Get Started <MdArrowOutward className="text-xl" />
-          </span>
-        </button>
-      </div>
+      <LandingNavbar/>
+      <HeroSection />
 
-      {/* Image with gradient */}
-      <div className="w-screen h-screen">
-        <img src={bgGradient} alt="" className="w-full absolute" />
-        <div className="relative flex justify-center top-[335px]">
-          <div className="p-3 border border-gray rounded-xl inline-block w-[1000px]">
+      <div className="w-screen">
+        <img src={bgGradient} alt="" className="w-full absolute top-[100px]" />
+        <div className="relative flex justify-center top-[200px]">
+          <div className="p-3 border border-gray rounded-xl w-[1000px]">
             <img src={dashboard} alt="" className="rounded-xl" />
           </div>
         </div>
@@ -59,54 +44,25 @@ const LandingPage = () => {
             account and start trading immediately.
           </div>
         </div>
-
-        <div
-          className="p-5 rounded-xl
-        shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-        >
-          <div className="flex justify-center mt-5">
-            <div className="w-14 h-14 bg-[#101636] rounded-full">
-              <img src={transaction} alt="" className="w-10 h-10 mt-2 ml-2" />
-            </div>
-          </div>
-
-          <div className="text-white text-xl font-sora text-center my-3">
-            Instant Deposits
-          </div>
-          <div className="text-gray text-center text-xs font-sora mb-5">
-            Immediate transfer of funds into user's account, allowing them to
-            start trading or investing without delay
-          </div>
-        </div>
-
-        <div
-          className="p-5 rounded-xl
-        shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-        >
-          <div className="flex justify-center mt-5">
-            <div className="w-14 h-14 bg-[#101636] rounded-full">
-              <img src={transaction} alt="" className="w-10 h-10 mt-2 ml-2" />
-            </div>
-          </div>
-
-          <div className="text-white text-xl font-sora text-center my-3">
-            Instant Withdrawals
-          </div>
-          <div className="text-gray text-center text-xs font-sora mb-5">
-            Rapid processing of fund withdrawals, enabling users to access their
-            money almost instantly after requesting a transfer.
-          </div>
-        </div>
+        <TransactionInfo
+          icon={transaction}
+          title="Instant Deposits"
+          description="Immediate transfer of funds into user's account, allowing them to start trading or investing without delay"
+        />
+        <TransactionInfo
+          icon={transaction}
+          title="Instant Withdrawals"
+          description="Rapid processing of fund withdrawals, enabling users to access their money almost instantly after requesting a transfer."
+        />
       </div>
 
-      {/* Explore trading oppurtunities */}
+      {/* Explore trading opportunities */}
       <div className="mt-20 w-screen">
         <div className="flex justify-center">
           <div className="text-white font-sora text-5xl text-center mt-14 w-1/2 ">
             Explore a World of Trading Opportunities
           </div>
         </div>
-
         <div className="flex justify-center">
           <div className="text-gray font-sora text-sm text-center mt-4 w-1/2 ">
             Whether you're into sports, politics, or global events, we have the
@@ -114,140 +70,59 @@ const LandingPage = () => {
             your knowledge thrives!
           </div>
         </div>
-
         <div className="flex justify-center">
-          <div className="grid grid-cols-2 w-1/2 gap-10 mt-20 ">
-            <div
-              className="rounded-xl  p-10
-            shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-            >
-              <div className="text-white text-2xl font-sora my-5">Sports</div>
-              <div className="text-gray font-sora text-xs mb-10">
-                Trade on your predictions for major sporting events, from team
-                performances to individual athlete achievements.
-              </div>
-              <div>
-                <img src={sports} alt="" className="h-52 w-52 mb-5" />
-              </div>
-            </div>
-
-            <div
-              className="rounded-xl p-10
-            shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-            >
-              <div className="text-white text-2xl font-sora my-5 ml-5">
-                Entertainment
-              </div>
-              <div className="text-gray font-sora text-xs mb-10">
-                Predict outcomes in the world of TV, movies, and celebrity
-                trends as they unfold in real time.
-              </div>
-              <div>
-                <img src={socialMedia} alt="" className="h-52 w-52 mb-5 ml-5" />
-              </div>
-            </div>
-
-            <div
-              className="rounded-xl  p-10
-            shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-            >
-              <div className="text-white text-2xl font-sora my-5">Economy</div>
-              <div className="text-gray font-sora text-xs mb-10">
-                Make informed trades based on key economic indicators, financial
-                trends, and market movements shaping the global landscape.
-              </div>
-              <div>
-                <img src={economy} alt="" className="h-52 w-52 mb-5" />
-              </div>
-            </div>
-
-            <div
-              className="rounded-xl p-10
-            shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-            >
-              <div className="text-white text-2xl font-sora my-5">Politics</div>
-              <div className="text-gray font-sora text-xs mb-10">
-                Engage in markets centered around political events, elections,
-                and policy developments worldwide.
-              </div>
-              <div>
-                <img src={politics} alt="" className="h-52 w-52 mb-5" />
-              </div>
-            </div>
+          <div className="grid grid-cols-2 px-52 gap-10 mt-20 ">
+            <TradingOpportunity
+              title="Sports"
+              description="Trade on your predictions for major sporting events, from team performances to individual athlete achievements."
+              image={sports}
+            />
+            <TradingOpportunity
+              title="Entertainment"
+              description="Predict outcomes in the world of TV, movies, and celebrity trends as they unfold in real time."
+              image={socialMedia}
+            />
+            <TradingOpportunity
+              title="Economy"
+              description="Make informed trades based on key economic indicators, financial trends, and market movements shaping the global landscape."
+              image={economy}
+            />
+            <TradingOpportunity
+              title="Politics"
+              description="Engage in markets centered around political events, elections, and policy developments worldwide."
+              image={politics}
+            />
           </div>
         </div>
       </div>
 
-      {/* Explore trading oppurtunities */}
+      {/* Maximize Your Trading Success */}
       <div className="mt-20 w-screen">
         <div className="flex justify-center">
           <div className="text-white font-sora text-5xl text-center mt-14 w-1/2 ">
             Maximize Your Trading Success with Cutting-Edge Tools!
           </div>
         </div>
-
         <div className="flex justify-center">
           <div className="text-gray font-sora text-sm text-center mt-4 w-1/2 ">
             Elevate your trading game with our powerful Orderbook and
             Probability Graph.
           </div>
         </div>
-
         <div className="flex justify-center">
-          <div className="grid grid-cols-2 w-1/2 gap-10 mt-20 ">
-            <div
-              className="rounded-xl  p-10
-            shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-            >
-              <img src={orderBook} alt="" className="h-52 w-52 mb-5" />
-            </div>
-
-            <div>
-              <div className="text-white text-2xl font-sora my-5 mt-10">
-                Orderbook
-              </div>
-              <div className="text-gray font-sora text-xs mb-10">
-                A real-time display of all buy and sell orders, providing
-                transparency into market activity and liquidity.
-              </div>
-              <div>
-                <button
-                  onClick={() => {}}
-                  className="bg-blue-500 text-white font-semibold rounded-[10px] font-sora px-5 py-3"
-                >
-                  <span className="flex gap-2">
-                    Learn more <MdArrowOutward className="text-xl" />
-                  </span>
-                </button>
-              </div>
-            </div>
-
-            <div>
-              <div className="text-white text-2xl font-sora my-5 mt-10">
-                Probability Graph
-              </div>
-              <div className="text-gray font-sora text-xs mb-10">
-                A visual representation of changing probabilities over time,
-                helping you track and analyze trends in event outcomes.
-              </div>
-              <div>
-                <button
-                  onClick={() => {}}
-                  className="bg-blue-500 text-white font-semibold rounded-[10px] font-sora px-5 py-3"
-                >
-                  <span className="flex gap-2">
-                    Learn more <MdArrowOutward className="text-xl" />
-                  </span>
-                </button>
-              </div>
-            </div>
-
-            <div
-              className="rounded-xl  p-10
-            shadow-[inset_-10px_-10px_30px_rgba(28,36,76,0.5),_inset_10px_10px_40px_rgba(28,36,76,0.5)]"
-            >
-              <img src={graph} alt="" className="h-52 w-52 mb-5" />
-            </div>
+          <div className="grid grid-cols-2 px-52 gap-10 mt-20 ">
+            <TradingTool
+              title="Orderbook"
+              description="A real-time display of all buy and sell orders, providing transparency into market activity and liquidity."
+              image={orderBook}
+              isReversed={false}
+            />
+            <TradingTool
+              title="Probability Graph"
+              description="A visual representation of changing probabilities over time, helping you track and analyze trends in event outcomes."
+              image={graph}
+              isReversed={true}
+            />
           </div>
         </div>
       </div>
@@ -259,156 +134,28 @@ const LandingPage = () => {
             Trusted by 14,000+ users
           </div>
         </div>
-
         <div className="flex justify-center">
           <div className="text-gray font-sora text-sm text-center mt-4 w-1/2 ">
             Sharing Their Trading Experiences
           </div>
         </div>
-
         <div className="grid grid-cols-3 gap-10 px-32">
-          <div className="border-2 border-white mt-10 rounded-[30px] ">
-            <div className="text-white font-gray font-sora py-10 px-5">
-              "I love using this platform! The user interface is intuitive, and
-              the real-time updates keep me engaged. The advanced tools,
-              especially the Probability Graph, have significantly improved my
-              trading strategies. Highly recommend it to anyone serious about
-              trading!"
-            </div>
-
-            <div className="flex justify-between">
-              <div>
-                <div className="text-white font-gray font-sora px-5 mt-2">
-                  Sarah Taranian
-                </div>
-                <div className="text-gray font-gray font-sora px-5">
-                  Creative Director
-                </div>
-              </div>
-
-              <div>
-                <img src={sports} alt="" className="h-20 w-20 rounded-[20px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="border-2 border-white mt-10 rounded-[30px] ">
-            <div className="text-white font-gray font-sora py-10 px-5">
-              "I love using this platform! The user interface is intuitive, and
-              the real-time updates keep me engaged. The advanced tools,
-              especially the Probability Graph, have significantly improved my
-              trading strategies. Highly recommend it to anyone serious about
-              trading!"
-            </div>
-
-            <div className="flex justify-between">
-              <div>
-                <div className="text-white font-gray font-sora px-5 mt-2">
-                  Sarah Taranian
-                </div>
-                <div className="text-gray font-gray font-sora px-5">
-                  Creative Director
-                </div>
-              </div>
-
-              <div>
-                <img src={sports} alt="" className="h-20 w-20 rounded-[20px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="border-2 border-white mt-10 rounded-[30px] ">
-            <div className="text-white font-gray font-sora py-10 px-5">
-              "I love using this platform! The user interface is intuitive, and
-              the real-time updates keep me engaged. The advanced tools,
-              especially the Probability Graph, have significantly improved my
-              trading strategies. Highly recommend it to anyone serious about
-              trading!"
-            </div>
-
-            <div className="flex justify-between">
-              <div>
-                <div className="text-white font-gray font-sora px-5 mt-2">
-                  Sarah Taranian
-                </div>
-                <div className="text-gray font-gray font-sora px-5">
-                  Creative Director
-                </div>
-              </div>
-
-              <div>
-                <img src={sports} alt="" className="h-20 w-20 rounded-[20px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="border-2 border-white mt-10 rounded-[30px] ">
-            <div className="text-white font-gray font-sora py-10 px-5">
-              "I love using this platform! The user interface is intuitive, and
-              the real-time updates keep me engaged. The advanced tools,
-              especially the Probability Graph, have significantly improved my
-              trading strategies. Highly recommend it to anyone serious about
-              trading!"
-            </div>
-
-            <div className="flex justify-between">
-              <div>
-                <div className="text-white font-gray font-sora px-5 mt-2">
-                  Sarah Taranian
-                </div>
-                <div className="text-gray font-gray font-sora px-5">
-                  Creative Director
-                </div>
-              </div>
-
-              <div>
-                <img src={sports} alt="" className="h-20 w-20 rounded-[20px]" />
-              </div>
-            </div>
-          </div>
-
-          <div className="border-2 border-white mt-10 rounded-[30px] ">
-            <div className="text-white font-gray font-sora py-10 px-5">
-              "I love using this platform! The user interface is intuitive, and
-              the real-time updates keep me engaged. The advanced tools,
-              especially the Probability Graph, have significantly improved my
-              trading strategies. Highly recommend it to anyone serious about
-              trading!"
-            </div>
-
-            <div className="flex justify-between">
-              <div>
-                <div className="text-white font-gray font-sora px-5 mt-2">
-                  Sarah Taranian
-                </div>
-                <div className="text-gray font-gray font-sora px-5">
-                  Creative Director
-                </div>
-              </div>
-
-              <div>
-                <img src={sports} alt="" className="h-20 w-20 rounded-[20px]" />
-              </div>
-            </div>
-          </div>
+          {[...Array(5)].map((_, index) => (
+            <CustomerReview
+              key={index}
+              review="I love using this platform! The user interface is intuitive, and the real-time updates keep me engaged. The advanced tools, especially the Probability Graph, have significantly improved my trading strategies. Highly recommend it to anyone serious about trading!"
+              name="Sarah Taranian"
+              position="Creative Director"
+              image={sports}
+            />
+          ))}
         </div>
       </div>
+
+
+      <Footer/>
     </div>
   );
 };
 
 export default LandingPage;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
