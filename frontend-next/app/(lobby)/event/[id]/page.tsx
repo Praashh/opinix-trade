efea5eefa136034571b1c713947f0ef4bb67282b
Prashant Varma <prashantvarma5083@gmail.com>
2024-10-03 00:28:29 +0530

    feat: event and orderbook+chart

--- frontend-next/app/(lobby)/event/[id]/page.tsx
+++ frontend-next/app/(lobby)/event/[id]/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 import Event from '@/app/components/Event'
 import { useParams } from 'next/navigation'
- 
+/*
+  TODO: 
+    1. server actiosn to fetch the event from the event id (not sure but for now).
+    2. Add sources of truth.
+    3. Add event details like name and all.
+*/
 export default function Page() {
   const router = useParams()
-  console.log(router.id);
-  
+  console.log(router);
   return <div>
     <Event/>
   </div>
